12b26d095334b626755c0d2ba7fd1645fa29096a
merdernoty <zimigkorsi99@gmail.com>
2025-10-16 21:09:58 +0300

    slices

---
+++ slice_and_array/iteration-optimization/comparison_test.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"testing"
+)
+
+const size = 1 << 10
+
+// go test -bench=. comparison_test.go
+
+func BenchmarkWithoutOptimization(b *testing.B) {
+	data := make([]int, size)
+	for i := 0; i < b.N; i++ {
+		for j := 0; j < len(data); j++ {
+			data[j] = i
+		}
+	}
+}
+
+func BenchmarkWithCalculatedLength(b *testing.B) {
+	data := make([]int, size)
+	for i := 0; i < b.N; i++ {
+		length := len(data)
+		for j := 0; j < length; j++ {
+			data[j] = 1
+		}
+	}
+}
+
+func BenchmarkWithLoopUnwinding(b *testing.B) {
+	data := make([]int, size)
+	for i := 0; i < b.N; i++ {
+		for j := 0; j < len(data)/4; j += 4 {
+			data[j] = i
+			data[j+1] = i
+			data[j+2] = i
+			data[j+3] = i
+		}
+	}
+}
